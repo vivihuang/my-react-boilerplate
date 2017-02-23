b018b8b982d85818a24429f323df477d14a1bd0c
Vivi Huang <rwhuang@thoughtworks.com>
2017-02-23 17:14:33 +0800

    Remove unused dependencies.

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import { reducer as formReducer } from 'redux-form'
 
 import activeRequestsReducer from './modules/activeRequestsReducer'
 
 export default combineReducers({
   routing: routerReducer,
-  form: formReducer,
   activeRequests: activeRequestsReducer
 })
