d3b66665789400366c9818ac2347535564c7272b
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-23 17:36:33 +0800

    Refactor layout component and requests reducer to show loading status.

--- src/app/Frame/Layout.js
+++ src/app/Frame/Layout.js
@@ -1,60 +1,30 @@
 import React, { PropTypes, Component } from 'react'
 import { connect } from 'react-redux'
-import autobind from 'autobind-decorator'
 import classnames from 'classnames'
-import Immutable from 'immutable'
+import { withRouter } from 'react-router-dom'
+
 import Loader from '../../components/Loader'
 
 import styles from './Layout.scss'
 
 const mapStateToProps = state => ({
-  activeRequests: state.activeRequests
+  activeRequests: state.activeRequests.toJS()
 })
 
 @connect(mapStateToProps)
 class Layout extends Component {
   static propTypes = {
     children: PropTypes.element,
-    activeRequests: PropTypes.instanceOf(Immutable.Map).isRequired
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      hasLoading: false
-    }
-    this.timer = null
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { activeRequests } = this.props
-    const currentTotal = activeRequests.get('total')
-    const nextTotal = nextProps.activeRequests.get('total')
-    if (!currentTotal && nextTotal && !this.timer) {
-      this.timer = window.setTimeout(() => {
-        this.setLoadingState(true)
-      }, 1000)
-    }
-    if (!nextTotal) {
-      if (this.timer) {
-        window.clearTimeout(this.timer)
-        this.timer = null
-      }
-      this.setLoadingState(false)
-    }
-  }
-
-  @autobind
-  setLoadingState(bool) {
-    this.setState({ hasLoading: bool })
+    activeRequests: PropTypes.shape({})
   }
 
   render() {
-    const { hasLoading } = this.state
+    const { activeRequests, children } = this.props
+    const hasLoading = !!activeRequests.total
 
     return (
       <div className={classnames('page-stage-layout')}>
-        { React.cloneElement(this.props.children, {
+        { React.cloneElement(children, {
           className: classnames({
             [styles.loadingOpen]: hasLoading
           })
@@ -65,4 +35,4 @@ class Layout extends Component {
   }
 }
 
-export default Layout
+export default withRouter(Layout)
--- src/redux/modules/activeRequestsReducer.js
+++ src/redux/modules/activeRequestsReducer.js
@@ -8,21 +8,18 @@ const initialState = Immutable.fromJS({
 export default function activeRequestsReducer(state = initialState, action = null) {
   if (action.meta && action.meta.API_ACTION) {
     const API_ACTION = action.meta.API_ACTION
-
-    const calculate = (state) => {
-      const total = state.get('data').reduce((prev, item) => prev + item, 0)
-      return state.set('total', total)
+    const calculate = (calculateState) => {
+      const total = calculateState.get('data').reduce((prev, item) => prev + item, 0)
+      return calculateState.set('total', total)
     }
-
-    const { API_REQUEST_START, API_REQUEST_FINISH, endpoint, method } = API_ACTION
-
+    const {
+      API_REQUEST_START, API_REQUEST_FINISH, endpoint, method
+    } = API_ACTION
     const key = `${method}-${endpoint}`
-
     if (API_REQUEST_START) {
       const count = state.getIn(['data', key]) || 0
       return calculate(state.setIn(['data', key], count + 1))
     }
-
     if (API_REQUEST_FINISH) {
       const count = state.getIn(['data', key]) || 1
       return calculate(state.setIn(['data', key], count - 1))
