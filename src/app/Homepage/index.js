d70a69fd80f80be80077fbbd85f65af40f65e5d7
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-24 11:36:01 +0800

    Add Login and Homepage component to test routers.

---
+++ src/app/Homepage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => (
+  <div>
+    <h2>Hi, welcome to homepage!</h2>
+  </div>
+)
+
+export default Homepage
---
+++ src/app/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => (
+  <div>Please login in first.</div>
+)
+
+export default Login
