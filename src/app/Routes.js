c00ad2cf7c3907544f2ba20ec5ca684fc23b7571
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-24 11:36:31 +0800

    Wrap authenticate component to routes.

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -2,18 +2,27 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Layout from './Frame/Layout'
 import Frame from './Frame/Frame'
+import AuthenticatedComponent from './Frame/AuthenticatedComponent'
+import Homepage from './Homepage'
+import Login from './Login'
 import NotFound from './NotFound'
 
 export default (
-  <Route>
-    <Layout>
-      <Route path='/'>
-        <Frame>
-          <Switch>
-            <Route component={NotFound} />
-          </Switch>
-        </Frame>
-      </Route>
-    </Layout>
-  </Route>
+  <Layout>
+    <Switch>
+      <Route path='/login' component={AuthenticatedComponent(Login, { auth: false })} />
+      <Route
+        path='/'
+        render={() => {
+          const AuthFrame = AuthenticatedComponent(Frame)
+          return (<AuthFrame>
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route component={NotFound} />
+            </Switch>
+          </AuthFrame>)
+        }}
+      />
+    </Switch>
+  </Layout>
 )
