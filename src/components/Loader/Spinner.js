3a243b3971965c6feba7969dcc9240c8e07384ac
Vivi Huang <rwhuang@thoughtworks.com>
2017-06-21 16:00:02 +0800

    Refactor loader component.

--- src/components/Loader/Spinner.js
+++ src/components/Loader/Spinner.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
 
-import Icon from '../Icon'
 import styles from './Spinner.scss'
 
-const Spinner = ({ className }) => (
-  <span className={classnames(styles.SpinnerV2, className)}>
-    <span className={styles.SpinnerIcon1}>
-      <span className={styles.innerIcon}>
-        <Icon icon='logo' className={styles.icon} />
-      </span>
-    </span>
-    <span className={styles.SpinnerIcon2}>
-      <span className={styles.innerIcon}>
-        <Icon icon='logo' className={styles.icon} />
-      </span>
-    </span>
-  </span>
+const Spinner = () => (
+  <div className={styles.ferry}>
+    <div className={styles.chimney} />
+    <div className={styles.waves} />
+  </div>
 )
-
-Spinner.propTypes = {
-  className: PropTypes.string
-}
 
 export default Spinner
